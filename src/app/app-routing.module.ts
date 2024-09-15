c517b9ee5f0210cb79388aefd52aa2b48a8bbdb6
gabyto19 <gabuniagoga19@gmail.com>
2024-09-15 15:32:54 +0400

    starting mobile version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './order/welcome/welcome.component';
 
 const routes: Routes = [];
 
